a1f140374f4e445097d904d0782031437a2a36ea
a044526721d <a044526721d@s09-pc20.ausias.lan>
2018-10-02 17:29:08 +0200

    Empezamos

---
+++ src/main/java/net/alejandroquevedo/p01cs/control.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.alejandroquevedo.p01cs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author a044526721d
+ */
+public class control extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            int fila = Integer.parseInt(request.getParameter("ancho"));
+            int columna = Integer.parseInt(request.getParameter("alto"));
+            int i, x;
+            ArrayList<Integer> arrayfila = new ArrayList<Integer>();
+            ArrayList<Object> arraycolumna = new ArrayList<Object>();
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">\n"
+                    + "");
+            out.println("<title>Servlet control</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<div class=\"container\">");
+            out.println("<h1>Tabla de multiplicar:</h1>");
+            out.println("</div>");
+            out.println("<div class=\"container\">");
+            out.println("<table class=\"table\" table-bordered float: none>");
+            for (i = 1; i <= columna; i++) {
+                out.println("<tr class=\"success\">");
+                for (x = 1; x <= fila; x++) {
+                    arrayfila = new ArrayList();
+                    arrayfila.add(i * x);
+                    out.println("<td class=\"text-center\">" + x * i + "</td>");
+                }
+                arraycolumna.add(arrayfila);
+                out.println("</tr>");
+            }
+            out.println("</table>");
+            out.println("</div>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
